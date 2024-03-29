c89151acb526b9895dfae1414e9cb3495e291b79
Spangled <133797996+bedwaring@users.noreply.github.com>
2024-03-29 20:31:55 +0300

    Delete path.ps1

--- path.ps1
+++
@@ -1,6 +0,0 @@
-$userPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-$dir = "%USERPROFILE%\.blueprint"
-$userPath += ";$dir"
-
-[System.Environment]::SetEnvironmentVariable("Path", $userPath, "User")
